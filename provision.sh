ced5c1b19fad2f3ed1ba2d92d81aaf482d301b9b
Thien Tran <thientran1986@gmail.com>
2016-12-26 16:43:34 +0700

    Revert "Add packer file. Update vagrant file & Provision.sh"
    This reverts commit 566c6acb9aaa58dfb110a310c4b6ae29cdd9deec.

--- provision.sh
+++ provision.sh
@@ -6,9 +6,6 @@ HOME_SERVERS_DIR=$HOME_DIR/servers
 HOME_PUBLIC_HTML_DIR=$HOME_DIR/public_html
 HOME_BIN_DIR=$HOME_DIR/bin
 
-nodejs="node-v7.3.0-linux-x64.tar.gz"
-nodejsdir="node-v7.3.0-linux-x64"
-
 installPackage()
 {
   local packages=$*
@@ -30,22 +27,10 @@ download()
   wget --progress=dot $3 $url
 }
 
-
-createDirs()
-{
-  echo 'Creating directories'
-  indent; echo 'Creating bin directory'
-  mkdir $HOME_BIN_DIR
-  indent; echo 'Creating public_html directory'
-  mkdir $HOME_PUBLIC_HTML_DIR
-  chmod o+xr $HOME_PUBLIC_HTML_DIR
-  mkdir $HOME_SERVERS_DIR
-  indent; echo 'Creating servers directory'
-}
-
 downloadSoftwares()
 {
     #download nodejs
+    nodejs="node-v7.3.0-linux-x64.tar.gz"
     if [ ! -e $nodejs ] 
         then
             indent; echo "There is no $nodejs"
@@ -57,25 +42,22 @@ downloadSoftwares()
 }
 
 run() {
-    createDirs
     cd $VAGRANT_DIR
     downloadSoftwares
     
     cd $HOME_BIN_DIR
     #install nodejs
     indent; echo "Extracting $file"
-    tar xvzf $nodejs >/dev/null 2>&1
+    tar xvzf $nodejs
     indent; echo 'Cleaning'
     rm $nodejs
   
     #update bashrc
-    indent; echo 'Updating .bashrc'
+    echo 'Updating .bashrc'
     cat $VAGRANT_DIR/bashrc.template >> $HOME_DIR/.bashrc
     source $HOME_DIR/.bashrc
     
     #install yeoman
-    indent; echo 'Installing yeoman'
-    cd $HOME_BIN_DIR/$nodejsdir/bin/
     npm install -g yo
 }
 
